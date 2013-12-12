c08eb060cdaeb407822ffd3747be965c01402977
Chris Wilkes <cwilkes@bluekai.com>
2013-12-12 10:43:42 -0800

    surf and hough lines in cam

--- cam_surf.py
+++ cam_surf.py
@@ -1,4 +1,6 @@
 import cv2
+import sys
+import numpy as np
 
 #Create object to read images from camera 0
 cam = cv2.VideoCapture(0)
@@ -9,11 +11,17 @@ surf = cv2.SURF(85)
 #Set desired radius
 rad = 2
 
-while True:
-    #Get image from webcam and convert to greyscale
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def do_hough_lines(img, gray):
+    edges = cv2.Canny(gray,50,150,apertureSize = 3)
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength, maxLineGap)
+    for x1, y1, x2, y2 in lines[0]:
+        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+
+def do_surf(img, gray):
     #Detect keypoints and descriptors in greyscale image
     #keypoints, descriptors = surf.detect(gray)
     keypoints = surf.detect(gray)
@@ -25,10 +33,23 @@ while True:
         y = int(kp.pt[1])
         cv2.circle(img, (x, y), rad, (0, 0, 255))
 
-    #Display colour image with detected features
-    cv2.imshow("features", img)
 
-    #Sleep infinite loop for ~10ms
-    #Exit if user presses <Esc>
-    if cv2.waitKey(10) == 27:
-        break
+def main(args):
+    if len(args) <= 1:
+        action = do_surf
+    else:
+        action = do_hough_lines
+    while True:
+        #Get image from webcam and convert to greyscale
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        action(img, gray)
+        cv2.imshow("features", img)
+        #Sleep infinite loop for ~10ms
+        #Exit if user presses <Esc>
+        if cv2.waitKey(10) == 27:
+            break
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
